2c076330ae1b8a7b27efc8e98e73c91c749b7c7c
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2026-08-19 11:28:01 -0700

    replace sqlite with postgres

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db("DATABASE_URL"),
 }
 
 
