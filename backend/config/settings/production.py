7e9de3ddd2be114c28d1ed8a78eb28f5a16d8d44
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2026-08-21 12:40:47 -0700

    infra(backend): initial deployment config (#17)

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,5 +1,27 @@
 from .base import *
 
+DEBUG = False
+
+# Security
+# https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])  # pyright: ignore[reportArgumentType]
+
+# https://render.com/docs/deploy-django#creating-a-new-django-project
+RENDER_EXTERNAL_HOSTNAME = env.str("RENDER_EXTERNAL_HOSTNAME", default=None)  # pyright: ignore[reportArgumentType]
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Start as short as possible and ramp up to the standard 31536000 (1 year)
+# once HTTPS is confirmed stable in production.
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
 # Email
 # https://pypi.org/project/django-anymail/
 
---
+++ docs/infrastructure.md
@@ -0,0 +1,5 @@
+# Infrastructure
+
+- Services and database hosted on [Render](https://render.com/), defined via [render.yaml](../render.yaml)
+- Email Service via [Resend](https://resend.com/)
+- Root Domain is `cardcase.jtm-dev.com`, managed via [Cloudflare](https://www.cloudflare.com/)
