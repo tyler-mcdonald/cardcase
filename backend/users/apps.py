40496b92fc89996fcc725117162339c80435339c
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2026-08-25 16:58:54 -0700

    docs(backend): explain why checks is imported in ready()

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -5,4 +5,5 @@ class UsersConfig(AppConfig):
     name = "users"
 
     def ready(self):
+        # Import 'checks' here to call the 'register()' decorator in that file.
         from users import checks  # noqa: F401
