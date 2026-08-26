d83f2eece8706f34a3bd809fd3c5ab803bccae5d
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2026-08-26 12:02:50 -0700

    chore(backend): drop unused ModelBackend from auth backends

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -155,7 +155,6 @@ MAILERS = {
 AUTH_USER_MODEL = "users.User"
 
 AUTHENTICATION_BACKENDS = [
-    "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
